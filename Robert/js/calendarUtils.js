6ee5a1d419cbc34a888ebe38b2f2927a65976611
roca5461 <robert.carlsson.5461@student.uu.se>
2017-04-13 11:07:50 +0200

    Struktur och simpel visuellt
    Struktur och visuell visning av columnerna

---
+++ Robert/js/calendarUtils.js
@@ -0,0 +1,9 @@
+// TODO: Setting the scroll_sizes of the hor and ver scroll
+Calendar.prototype._set_scroll_size = function(){
+  /*
+  */
+};
+
+// TODO: Calculate the scroll_sizes for the task and resources
+Calendar.prototype._get_scroll_sizes = function(){
+};
