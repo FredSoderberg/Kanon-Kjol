ea3dcbfd06e8317588fcbc66d1b45eb9ff0c690f
Fredrik Söderberg <soderbergh@gmail.com>
2017-04-18 16:17:42 +0200

    mihihhi

--- Fredrik/scripts.dB/dB_util.js
+++ Fredrik/scripts.dB/dB_util.js
@@ -1,50 +1,69 @@
+function removeTask(task) {
+console.log("data1: " + task);
+    taskSend = JSON.stringify(task);
+    console.log("data2: " + taskSend);
 
+    $.post('scripts.dB/dB_util.php', {
+            function: "removeTask",
+            taskToRemove: taskSend
+        })
 
+        .done(function(data) {
+      //      console.log("datan:" + data);
+            listAllTasks();
+        })
 
-function insertTask(id,task) {
-//console.log("data2: "+task);
-if (id==null && name==null) {
-
+        .fail(function(jqxhr, textStatus, error) {
+            var err = textStatus + ", " + error;
+        //    console.log("Request Failed: " + err);
+        });
 }
-else if (id==null && task !=null) {
-  $.post('scripts.dB/dB_util.php', {function:"insertTask",newTask:task} )
 
-      .done(function(data) {
-  //      console.log("datan:"+data);
-        listAllTasks();
-       })
+function insertTask(task) {
+//    console.log("data1: " + task);
+    taskSend = JSON.stringify(task);
+//    console.log("data2: " + taskSend);
 
-      .fail(function(jqxhr, textStatus, error) {
-          var err = textStatus + ", " + error;
-          console.log("Request Failed: " + err);
-      });
+    $.post('scripts.dB/dB_util.php', {
+            function: "insertTask",
+            newTask: taskSend
+        })
+
+        .done(function(data) {
+      //      console.log("datan:" + data);
+            listAllTasks();
+        })
+
+        .fail(function(jqxhr, textStatus, error) {
+            var err = textStatus + ", " + error;
+      //      console.log("Request Failed: " + err);
+        });
 }
 
-}
 
 
 
 
 
 function listAllTasks() {
-  $.getJSON('scripts.dB/dB_util_JSON.php', {function:"getAllTasks"} )
-
-      .done(function(tasks) {
-      $("#tasks_list").empty();
-          console.log(tasks);
-        $.each(tasks,function (key,val) {
-          $("#tasks_list").append("<li>ID: "+val.id+" - Name: "+val.name+"</li>");
+    $.getJSON('scripts.dB/dB_util_JSON.php', {
+            function: "getAllTasks"
         })
 
-       })
+        .done(function(tasks) {
+            $("#tasks_list").empty();
+            //  console.log(tasks);
+            $.each(tasks, function(key, val) {
+                $("#tasks_list").append("<li>ID: " + val.id + " - Name: " + val.name + "</li>");
+            })
 
-      .fail(function(jqxhr, textStatus, error) {
-          var err = textStatus + ", " + error;
-          console.log("Request Failed: " + err);
-      });
+        })
+
+        .fail(function(jqxhr, textStatus, error) {
+            var err = textStatus + ", " + error;
+            console.log("Request Failed: " + err);
+        });
 }
-
-
 
 
 
