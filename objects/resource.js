a7ba9a0298fe1f89d03a0fe67ba0c0afdba86496
erikajansson1 <erika_jansson1@hotmail.com>
2017-05-10 00:01:21 +0200

    Ändrade grundfärg på resources

--- objects/resource.js
+++ objects/resource.js
@@ -2,7 +2,7 @@ function Resource(id, name, groupType) {
   this.id          = id;
   this.name        = name;
   this.groupType   = groupType;
-  this.color       = "#AFA";
+  this.color       = "#f0f0f0";
   this.type        = "Resource";
   this.row;
   this.projectID;
